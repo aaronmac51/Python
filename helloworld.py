b632b7e81f3da4b26245ac514f823f718339206e
aaronmac51 <aaron.mcgeehon@gmail.com>
2019-11-25 11:15:36 -0700

    gfgdsf

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,9 @@
+list = ['word', 'sup', 1]
+
 def yolo():
-    return("yolos")
-
-yolo_output = yolo()
-
+    return(list[0])
+sup = 'sup'
+world = 'world'
+now = 'now'
+yolo_output = '{} and {} {}'.format(sup, world, now)
 print(yolo_output)
